a31724ace3608af605dc6db6c3d3fa3c077d4217
amirlwf <amirlwff@gamil.com>
2026-06-29 11:13:15 +0330

    fix config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "https://ehdwvdubcudlvbpbsvld.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ehdwvdubcudlvbpbsvld.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZHd2ZHViY3VkbHZicGJzdmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2ODg0NzIsImV4cCI6MjA5ODI2NDQ3Mn0.-6-UK8q-sOr38an2-EvEJXzt5Iaskm-T7VYhqR-zTEU";
